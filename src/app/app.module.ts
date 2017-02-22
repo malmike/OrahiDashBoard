a6441a3a54489445b4c7c4e552055fdb22c320a7
Male Michael <malmike21@outlook.com>
2017-02-22 22:35:03 +0300

    Added routing, modules and material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { AppAccessModule} from './modules/app-access.module';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    AppRoutingModule,
+    AppAccessModule
   ],
   providers: [],
   bootstrap: [AppComponent]
