055f8a89db1051ca9627b4f8017d97c89882c941
Male Michael <malmike21@outlook.com>
2017-02-25 11:16:14 +0300

    Added a http calls for login and registration

---
+++ src/app/services/httpCalls/login-sp.service.ts
@@ -0,0 +1,71 @@
+import { Injectable }    from '@angular/core';
+import { Headers, Http, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+import { GlobalVariables } from '../../global-variables/global-variables';
+import { ServiceProviderModel } from '../../models/service-provider.model';
+import { ResponseSpModel } from '../../models/response-sp.model';
+
+@Injectable()
+export class LoginSpService {
+
+    authUrl:string = GlobalVariables.getInstance().getWebApi();
+    private serviceProvider: ServiceProviderModel = new ServiceProviderModel();
+    private token: string;
+
+    constructor(private http: Http) {}
+
+    authenticate(serviceProvider: ServiceProviderModel, path: string): Observable<ResponseSpModel>{
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+        let requestoptions = new RequestOptions({
+            headers: headers
+        });
+
+
+        let urlPath: string = this.authUrl + path;
+
+        return this.http
+            .post(urlPath, JSON.stringify(serviceProvider), requestoptions)
+            .map((res: Response) => {          
+                let responseSp: ResponseSpModel = res.json();
+                if(responseSp.status === 'success'){
+                    this.serviceProvider = responseSp.serviceProvider;
+                    this.token = responseSp.token;
+                    localStorage.setItem('currentUser', JSON.stringify({ serviceProvider: this.serviceProvider, token: this.token }));
+                    return responseSp;
+                }else{
+                    return responseSp;
+                }
+            })
+            .catch((err) => this.handleError(err));
+    }
+
+    public handleError (error: Response | any) {
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || 'Failed';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
+
+    getServiceProvider():ServiceProviderModel{
+        return this.serviceProvider;
+    }
+
+    logout(): void {
+        // clear token remove user from local storage to log user out
+        this.token = null;
+        this.serviceProvider = null;
+        localStorage.removeItem('currentUser');
+    }
+
+}
+	
---
+++ src/app/services/httpCalls/register-sp.service.ts
@@ -0,0 +1,51 @@
+import { Injectable }    from '@angular/core';
+import { Headers, Http, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+import { GlobalVariables } from '../../global-variables/global-variables';
+import { ServiceProviderModel } from '../../models/service-provider.model';
+import { ResponseSpModel } from '../../models/response-sp.model';
+
+@Injectable()
+export class RegisterSpService {
+
+    authUrl:string = GlobalVariables.getInstance().getWebApi();
+
+    constructor(private http: Http) {}
+
+    register(serviceProvider: ServiceProviderModel, path: string): Observable<ResponseSpModel>{
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+        let requestoptions = new RequestOptions({
+            headers: headers
+        });
+        
+        let urlPath: string = this.authUrl + path;
+
+        return this.http
+                   .post(urlPath, JSON.stringify(serviceProvider), requestoptions)
+                   .map((res: Response) => {
+                       
+                       let responseSp: ResponseSpModel = res.json();
+                       return responseSp;
+                   })
+                   .catch((err) => this.handleError(err));
+    }
+
+    public handleError (error: Response | any) {
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || 'Failed';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
+
+}
+	
