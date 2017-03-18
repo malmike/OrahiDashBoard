9797f27ca332580e828f2d64a1dc488150a2500c
Male Michael <malmike21@outlook.com>
2017-03-18 17:11:07 +0300

    Added navigation menu data Added thenavigation data for the links that show on the navigation menu in the dashboard module

---
+++ src/app/app-data/nav-menu.data.ts
@@ -0,0 +1,6 @@
+import { NavMenuModel } from '../models/nav-menu.model';
+export const NavMenu:Array<NavMenuModel> = [
+    {link:'dashboard', label:'DashBoard'},
+    {link:'addservice', label:'Add Service'},
+    {link:'addservicecategory', label:'Add Service Category'}
+];
