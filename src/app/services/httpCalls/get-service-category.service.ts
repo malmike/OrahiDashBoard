eb89c1a93fa8e7b3168886151071d79ca52eae5d
Male Michael <malmike21@outlook.com>
2017-03-18 17:33:22 +0300

    Added service for getting service categories The service retrieves service categories from the web api

---
+++ src/app/services/httpCalls/get-service-category.service.ts
@@ -0,0 +1,61 @@
+import { Injectable }    from '@angular/core';
+import { Headers, Http, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+import { GlobalVariables } from '../../global-variables/global-variables';
+import { ServiceCategoryModel } from '../../models/service-category.model';
+
+@Injectable()
+export class GetServiceCategoryService {
+
+    authUrl:string = GlobalVariables.getInstance().getWebApi();
+    serviceCategoryList: Array<ServiceCategoryModel> = new Array<ServiceCategoryModel>();
+
+    constructor(private http: Http) {}
+
+    getServiceCategory(path: string, token: string): Observable<Boolean>{
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('x-access-token', token);
+        headers.append('Accept', 'application/json');
+        let requestoptions = new RequestOptions({
+            headers: headers
+        });
+
+
+        let urlPath: string = this.authUrl + path;
+
+        return this.http
+            .get(urlPath, requestoptions)
+            .map((res: Response) => {  
+                let test: Array<ServiceCategoryModel> = res.json();        
+                this.serviceCategoryList = test;
+                if(this.serviceCategoryList.length !== 0){
+                    return true;
+                }else{
+                    return false;
+                }
+            })
+            .catch((err) => this.handleError(err));
+    }
+
+    public handleError (error: Response | any) {
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || 'Failed';
+            const err = body.message || JSON.stringify(body);
+            errMsg = `${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
+
+    public getServiceCategoryList(): Array<ServiceCategoryModel>{
+        return this.serviceCategoryList;
+    }
+
+}
+	
