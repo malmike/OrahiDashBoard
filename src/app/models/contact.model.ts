d17339ddc53a3405d9da923ad05a0cf33480f529
Male Michael <malmike21@outlook.com>
2017-03-18 17:27:23 +0300

    Added a contact model Added a contact model that is to hold the contact information of the service provider

---
+++ src/app/models/contact.model.ts
@@ -0,0 +1,8 @@
+export class ContactModel{
+    constructor(
+        public name?: string,
+        public phoneNumber?: string,
+        public picture?: string,        
+        public email?: string
+    ){}
+}
