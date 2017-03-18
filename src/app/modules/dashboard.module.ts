8cfa04d97e60eb91eaa2ce736ed5d7d4c3b8db97
Male Michael <malmike21@outlook.com>
2017-03-18 17:06:30 +0300

    Added components, modules, routes and services Added to the dashboard module the components, modules, routes, directives and services that will be using it

--- src/app/modules/dashboard.module.ts
+++ src/app/modules/dashboard.module.ts
@@ -4,26 +4,51 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@angular/material';
 import { HttpModule } from '@angular/http';
 import { ReactiveFormsModule  } from '@angular/forms';
+import { AgmCoreModule } from 'angular2-google-maps/core';
 import 'hammerjs';
 
+//External components
+import { ImageCropperComponent } from 'ng2-img-cropper';
+
 //Components
+import { DashBoardComponent } from '../components/dashboard/dashboard.component';
+import { AppNavComponent } from '../components/app-navigation/app-nav.component';
+import { AddServiceComponent } from '../components/sp-services/add-service/add-service.component';
+import { AddCategoryComponent } from '../components/sp-services/add-category/add-category.component';
 
 //Routing Modules
 import { DashBoardRoutingModule } from '../routes/dashboard-routing.module';
 
 //Services
+import { NavMenuService } from '../services/shared-information/nav-menu.service';
+import { AddServiceService } from '../services/httpCalls/add-service.service';
+import { AddServiceCategoryService } from '../services/httpCalls/add-service-category.service';
+import { GetServiceCategoryService } from '../services/httpCalls/get-service-category.service';
 
 @NgModule({
     imports: [ 
         CommonModule, 
         HttpModule,
         ReactiveFormsModule ,
-        MaterialModule.forRoot(), 
+        MaterialModule.forRoot(),
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyCWWwrRN2N09Qt5TtJQTQi-4i0bi10LMSE'
+        }),
         DashBoardRoutingModule
     ],  
     declarations: [ 
+        AppNavComponent,
+        DashBoardComponent,
+        AddServiceComponent,
+        AddCategoryComponent,
+        ImageCropperComponent
     ],
     providers: [
-     ]
+        NavMenuService,
+        AddServiceService,
+        AddServiceCategoryService,
+        GetServiceCategoryService
+    ],
+    bootstrap: [AppNavComponent]
 })
-export class AppAccessModule { }
+export class DashBoardModule { }
