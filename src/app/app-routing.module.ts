e81b2ec155447ef01a7b8329a1d3082a7b1b26ea
Male Michael <malmike21@outlook.com>
2017-03-18 16:44:20 +0300

    Added app routing path for the dash board module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 export const routes: Routes = [
   {path: '', redirectTo: 'splogin', pathMatch: 'full'},
   {path: 'splogin', redirectTo: 'splogin', pathMatch: 'full'},
-  {path: 'spregister', redirectTo: 'spregister', pathMatch: 'full'}
+  {path: 'spregister', redirectTo: 'spregister', pathMatch: 'full'},
+  {path: 'nav', loadChildren: 'app/modules/dashboard.module#DashBoardModule'}
 ];
 
 @NgModule({
