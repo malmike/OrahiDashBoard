f63c096b7936a43c4ad9f86de93a533020b41485
Male Michael <malmike21@outlook.com>
2017-03-18 16:45:46 +0300

    Added extra paths used in the web api

--- src/app/app-data/webapi-path.data.ts
+++ src/app/app-data/webapi-path.data.ts
@@ -2,5 +2,8 @@ import { WebApiPathModel } from '../models/webapi-path.model';
 
 export const WebApiPathList:Array<WebApiPathModel> = [
     {name: 'register-sp', path: 'api/serviceProvider/registerServiceProvider'},
-    {name: 'login-sp', path: 'api/serviceProvider/authenticate'}
+    {name: 'login-sp', path: 'api/serviceProvider/authenticate'},
+    {name: 'add-service', path: 'api/serviceProvider/postService'},
+    {name: 'add-service-category', path: 'api/serviceProvider/addCategory'},
+    {name: 'get-service-category', path: 'api/serviceProvider/getCategories'}
 ]
