f83de7b365fbe43a918f8ae3cb3d81758bce2c40
Male Michael <malmike21@outlook.com>
2017-03-18 17:02:57 +0300

    Added routes to the dashboard routing module

--- src/app/routes/dashboard-routing.module.ts
+++ src/app/routes/dashboard-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { AppNavComponent } from '../components/app-navigation/app-nav.component';
+import { DashBoardComponent } from '../components/dashboard/dashboard.component';
+import { AddServiceComponent } from '../components/sp-services/add-service/add-service.component';
+import { AddCategoryComponent } from '../components/sp-services/add-category/add-category.component';
+
 @NgModule({
   imports: [RouterModule.forChild([
+    {path: '', component:AppNavComponent,
+      children:[
+        { path: '', redirectTo:'dashboard', pathMatch: 'full'},
+        { path: 'dashboard', component:DashBoardComponent },
+        { path: 'addservice', component:AddServiceComponent },
+        { path: 'addservicecategory', component:AddCategoryComponent }
+      ]
+    }
   ])],
   exports: [RouterModule]
 })
