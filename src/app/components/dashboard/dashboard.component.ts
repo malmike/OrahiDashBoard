bb3f31377fae1a50026884636fdc131547120709
Male Michael <malmike21@outlook.com>
2017-03-18 17:11:42 +0300

    Added the dash board component

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+
+export class DashBoardComponent{
+
+}
