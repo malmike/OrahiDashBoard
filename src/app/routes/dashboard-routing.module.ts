95399ac545ef1b1c4ad75487e1f62e1540ac002f
Male Michael <malmike21@outlook.com>
2017-06-07 16:29:21 +0300

    Made changes to the dash board routing module

--- src/app/routes/dashboard-routing.module.ts
+++ src/app/routes/dashboard-routing.module.ts
@@ -5,19 +5,23 @@ import { AppNavComponent } from '../components/app-navigation/app-nav.component'
 import { DashBoardComponent } from '../components/dashboard/dashboard.component';
 import { AddServiceComponent } from '../components/sp-services/add-service/add-service.component';
 import { AddCategoryComponent } from '../components/sp-services/add-category/add-category.component';
+import { SpReceiptsComponent } from '../components/sp-receipts/sp-receipts.component';
 
 @NgModule({
   imports: [RouterModule.forChild([
     {path: '', component:AppNavComponent,
       children:[
         { path: '', redirectTo:'dashboard', pathMatch: 'full'},
-        { path: 'dashboard', component:DashBoardComponent },
+        { path: 'dashboard', component:SpReceiptsComponent },
         { path: 'addservice', component:AddServiceComponent },
-        { path: 'addservicecategory', component:AddCategoryComponent }
+        { path: 'addservicecategory', component:AddCategoryComponent },
+        { path: 'receipts', component:SpReceiptsComponent}
       ]
     }
   ])],
   exports: [RouterModule]
 })
 export class DashBoardRoutingModule {}
+
+// { path: 'dashboard', component:DashBoardComponent }
 
