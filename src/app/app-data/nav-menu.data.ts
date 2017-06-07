6beaa97e46659acadd089f4f16f472c74e6cb52a
Male Michael <malmike21@outlook.com>
2017-06-07 16:20:39 +0300

    Added image icons to the nav menu data

--- src/app/app-data/nav-menu.data.ts
+++ src/app/app-data/nav-menu.data.ts
@@ -1,6 +1,7 @@
 import { NavMenuModel } from '../models/nav-menu.model';
 export const NavMenu:Array<NavMenuModel> = [
-    {link:'dashboard', label:'DashBoard'},
-    {link:'addservice', label:'Add Service'},
-    {link:'addservicecategory', label:'Add Service Category'}
+    {link:'dashboard', label:'DashBoard', image:'./assets/svg/ic_dashboard_black_24px.svg'},
+    {link:'addservice', label:'Add Service', image:'./assets/svg/ic_add_location_black_24px.svg'},
+    {link:'addservicecategory', label:'Add Service Category', image:'./assets/svg/ic_domain_black_24px.svg'},
+    {link:'receipts', label:'Receipts', image:'./assets/svg/ic_receipt_black_24px.svg'}
 ];
